b44a2ce4488244da9cdf8f8a6a64b70de75930ca
Maryam Anileleye <maryamanileleye@gmail.com>
2026-07-24 11:38:49 +0100

    Add Password reset functionality

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -149,3 +149,15 @@ LOGOUT_REDIRECT_URL = "home"
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
+# EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+# EMAIL_FILE_PATH = BASE_DIR / 'sent_emails'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config("EMAIL_HOST")               
+EMAIL_PORT = config("EMAIL_PORT")                            
+EMAIL_USE_TLS = config("EMAIL_USE_TLS")                                               
+EMAIL_HOST_USER = config('EMAIL_HOST_USER') 
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
